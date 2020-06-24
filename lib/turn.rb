4074af8ec64ccec6167fe4bc95a63652f026aeb7
Morgan Matney <morgan.matney89@gmail.com>
2020-06-24 00:24:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,7 +41,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
-  if valid_move?(board, index) == true
+  if valid_move?(board, input) == true
     return move(board, index, character = "X")
   else
     return turn(board)
